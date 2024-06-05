c2ea483001b3960e317f53f7cfe665c4cd98b9c2
51Sirius <71227522+51sirius@users.noreply.github.com>
2024-06-05 16:01:01 +0300

    second try

--- DiscordFS.py
+++ DiscordFS.py
@@ -1,133 +1,90 @@
-#!/usr/bin/env python
-
-from __future__ import with_statement
-
 import os
 import sys
-import errno
+import discord
+from fuse import FUSE, Operations
+import io
+from stat import S_IFDIR, S_IFREG
 
-from fuse import FUSE, FuseOSError, Operations
+TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
+GUILD_ID = 1
+ROOT_CHANNEL_ID = 1
+
+client = discord.Client()
 
 
-class Passthrough(Operations):
-    def __init__(self, root):
-        self.root = root
-
-    # Helpers
-    # =======
-
-    def _full_path(self, partial):
-        partial = partial.lstrip("/")
-        path = os.path.join(self.root, partial)
-        return path
-
-    # Filesystem methods
-    # ==================
-
-    def access(self, path, mode):
-        full_path = self._full_path(path)
-        if not os.access(full_path, mode):
-            raise FuseOSError(errno.EACCES)
-
-    def chmod(self, path, mode):
-        full_path = self._full_path(path)
-        return os.chmod(full_path, mode)
-
-    def chown(self, path, uid, gid):
-        full_path = self._full_path(path)
-        return os.chown(full_path, uid, gid)
+class DiscordFS(Operations):
+    def __init__(self):
+        self.channels = {}  # Сохранение каналов как директорий
 
     def getattr(self, path, fh=None):
-        full_path = self._full_path(path)
-        st = os.lstat(full_path)
-        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
-                     'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+        # Атрибуты файла или директории
+        st = dict(st_mode=(S_IFDIR | 0o755), st_nlink=2)
+        if path != '/':
+            st = dict(st_mode=(S_IFREG | 0o644), st_nlink=1, st_size=100)
+        return st
 
     def readdir(self, path, fh):
-        full_path = self._full_path(path)
-
-        dirents = ['.', '..']
-        if os.path.isdir(full_path):
-            dirents.extend(os.listdir(full_path))
-        for r in dirents:
-            yield r
-
-    def readlink(self, path):
-        pathname = os.readlink(self._full_path(path))
-        if pathname.startswith("/"):
-            # Path name is absolute, sanitize it.
-            return os.path.relpath(pathname, self.root)
-        else:
-            return pathname
-
-    def mknod(self, path, mode, dev):
-        return os.mknod(self._full_path(path), mode, dev)
-
-    def rmdir(self, path):
-        full_path = self._full_path(path)
-        return os.rmdir(full_path)
+        # Чтение содержимого директории
+        return ['.', '..'] + list(self.channels.keys())
 
     def mkdir(self, path, mode):
-        return os.mkdir(self._full_path(path), mode)
+        # Создание новой директории (канала)
+        dirname = path.strip('/')
+        client.loop.create_task(self.create_channel(dirname))
+        self.channels[dirname] = None
 
-    def statfs(self, path):
-        full_path = self._full_path(path)
-        stv = os.statvfs(full_path)
-        return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
-            'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
-            'f_frsize', 'f_namemax'))
+    def rmdir(self, path):
+        # Удаление директории (канала)
+        dirname = path.strip('/')
+        if dirname in self.channels:
+            client.loop.create_task(self.delete_channel(dirname))
+            del self.channels[dirname]
 
-    def unlink(self, path):
-        return os.unlink(self._full_path(path))
+    async def create_channel(self, name):
+        guild = client.get_guild(GUILD_ID)
+        return await guild.create_text_channel(name)
 
-    def symlink(self, name, target):
-        return os.symlink(name, self._full_path(target))
-
-    def rename(self, old, new):
-        return os.rename(self._full_path(old), self._full_path(new))
-
-    def link(self, target, name):
-        return os.link(self._full_path(target), self._full_path(name))
-
-    def utimens(self, path, times=None):
-        return os.utime(self._full_path(path), times)
-
-    # File methods
-    # ============
-
-    def open(self, path, flags):
-        full_path = self._full_path(path)
-        return os.open(full_path, flags)
+    async def delete_channel(self, name):
+        guild = client.get_guild(GUILD_ID)
+        channel = discord.utils.get(guild.channels, name=name)
+        await channel.delete()
 
     def create(self, path, mode, fi=None):
-        full_path = self._full_path(path)
-        return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
+        # Создание файла (отправка сообщения)
+        filename = path.strip('/')
+        client.loop.create_task(self.send_message(ROOT_CHANNEL_ID, f"New file created: {filename}"))
 
-    def read(self, path, length, offset, fh):
-        os.lseek(fh, offset, os.SEEK_SET)
-        return os.read(fh, length)
+    def write(self, path, data, offset, fh):
+        # Запись данных в файл (отправка сообщения с файлом)
+        filename = path.strip('/')
+        client.loop.create_task(self.send_message(ROOT_CHANNEL_ID, data, filename=filename))
+        return len(data)
 
-    def write(self, path, buf, offset, fh):
-        os.lseek(fh, offset, os.SEEK_SET)
-        return os.write(fh, buf)
+    def unlink(self, path):
+        # Удаление файла (удаление сообщения)
+        filename = path.strip('/')
+        client.loop.create_task(self.delete_message(ROOT_CHANNEL_ID, filename))
 
-    def truncate(self, path, length, fh=None):
-        full_path = self._full_path(path)
-        with open(full_path, 'r+') as f:
-            f.truncate(length)
+    async def send_message(self, channel_id, content, filename=None):
+        channel = client.get_channel(channel_id)
+        if filename:
+            file = discord.File(fp=io.BytesIO(content.encode()), filename=filename)
+            await channel.send(file=file)
+        else:
+            await channel.send(content)
 
-    def flush(self, path, fh):
-        return os.fsync(fh)
-
-    def release(self, path, fh):
-        return os.close(fh)
-
-    def fsync(self, path, fdatasync, fh):
-        return self.flush(path, fh)
+    async def delete_message(self, channel_id, filename):
+        channel = client.get_channel(channel_id)
+        async for message in channel.history():
+            if message.attachments and message.attachments[0].filename == filename:
+                await message.delete()
 
 
-def main(mountpoint, root):
-    FUSE(Passthrough(root), mountpoint, nothreads=True, foreground=True)
+def main(mountpoint):
+    FUSE(DiscordFS(), mountpoint, nothreads=True, foreground=True)
+
 
 if __name__ == '__main__':
-    main(sys.argv[2], sys.argv[1])
+    mountpoint = sys.argv[1]
+    client.loop.create_task(main(mountpoint))
+    client.run(TOKEN)
