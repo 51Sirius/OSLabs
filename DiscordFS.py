2a5a1654717cf49c1a592961e86be94a08795288
51Sirius <71227522+51sirius@users.noreply.github.com>
2024-06-07 03:09:57 +0300

    add ctime and mtime

--- DiscordFS.py
+++ DiscordFS.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import time
+
 import discord
 from fuse import FUSE, Operations
 import io
@@ -18,57 +20,64 @@ client = discord.Client(intents=intents)
 class DiscordFS(Operations):
     def __init__(self, loop):
         self.loop = loop
-        self.channels = {}  # Сохранение каналов как директорий
-        self.files = {}     # Сохранение файлов
+        self.channels = {}
+        self.files = {}
 
     def getattr(self, path, fh=None):
-        # Атрибуты файла или директории
         st = dict(st_mode=(S_IFDIR | 0o755), st_nlink=2)
         if path != '/':
-            st = dict(st_mode=(S_IFREG | 0o644), st_nlink=1, st_size=100)
+            filename = path.strip('/')
+            if filename in self.files:
+                file_info = self.files[filename]
+                st = dict(st_mode=(S_IFREG | 0o644), st_nlink=1, st_size=file_info['size'],
+                          st_ctime=file_info['ctime'], st_mtime=file_info['mtime'])
+            else:
+                raise FileNotFoundError(f"No such file: {filename}")
         return st
 
     def readdir(self, path, fh):
-        # Чтение содержимого директории
-        return ['.', '..'] + list(self.channels.keys())
+        return ['.', '..'] + list(self.channels.keys()) + list(self.files.keys())
 
     def mkdir(self, path, mode):
-        # Создание новой директории (канала)
         dirname = path.strip('/')
         asyncio.run_coroutine_threadsafe(self.create_channel(dirname), self.loop).result()
         self.channels[dirname] = None
 
     def rmdir(self, path):
-        # Удаление директории (канала)
         dirname = path.strip('/')
         if dirname in self.channels:
             asyncio.run_coroutine_threadsafe(self.delete_channel(dirname), self.loop).result()
             del self.channels[dirname]
 
     def create(self, path, mode, fi=None):
-        # Создание файла (отправка сообщения)
         filename = path.strip('/')
-        asyncio.run_coroutine_threadsafe(self.send_message(ROOT_CHANNEL_ID, f"New file created: {filename}"), self.loop).result()
+        asyncio.run_coroutine_threadsafe(self.send_message(ROOT_CHANNEL_ID, f"New file created: {filename}"),
+                                         self.loop).result()
 
     def write(self, path, data, offset, fh):
-        # Запись данных в файл (отправка сообщения с файлом)
         filename = path.strip('/')
-        message = asyncio.run_coroutine_threadsafe(self.send_message(ROOT_CHANNEL_ID, data, filename=filename), self.loop).result()
-        self.files[filename] = message.id  # Сохранение ID сообщения для последующего чтения
+        message = asyncio.run_coroutine_threadsafe(self.send_message(ROOT_CHANNEL_ID, data, filename=filename),
+                                                   self.loop).result()
+        ctime = time.mktime(message.created_at.timetuple())
+        self.files[filename] = {
+            'id': message.id,
+            'size': len(data),
+            'ctime': ctime,
+            'mtime': ctime
+        }
         return len(data)
 
     def read(self, path, size, offset, fh):
-        # Чтение данных из файла (получение содержимого сообщения)
         filename = path.strip('/')
-        message_id = self.files.get(filename)
-        if message_id:
-            content = asyncio.run_coroutine_threadsafe(self.get_message_content(ROOT_CHANNEL_ID, message_id), self.loop).result()
+        file_info = self.files.get(filename)
+        if file_info:
+            content = asyncio.run_coroutine_threadsafe(self.get_message_content(ROOT_CHANNEL_ID, file_info['id']),
+                                                       self.loop).result()
             return content[offset:offset + size]
         else:
             raise IOError("File not found")
 
     def unlink(self, path):
-        # Удаление файла (удаление сообщения)
         filename = path.strip('/')
         asyncio.run_coroutine_threadsafe(self.delete_message(ROOT_CHANNEL_ID, filename), self.loop).result()
         del self.files[filename]
@@ -107,20 +116,22 @@ class DiscordFS(Operations):
             if message.attachments and message.attachments[0].filename == filename:
                 await message.delete()
 
+
 def start_fuse(mountpoint, loop):
     fuse_operations = DiscordFS(loop)
     FUSE(fuse_operations, mountpoint, nothreads=True, foreground=True)
 
+
 if __name__ == '__main__':
     mountpoint = sys.argv[1]
+
 
     @client.event
     async def on_ready():
         print(f'Logged in as {client.user}')
-
-        # Запуск FUSE в отдельном потоке
         loop = asyncio.get_event_loop()
         fuse_thread = threading.Thread(target=start_fuse, args=(mountpoint, loop))
         fuse_thread.start()
+
 
     client.run(TOKEN)
